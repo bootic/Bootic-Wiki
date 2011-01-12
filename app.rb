1359e1311aa059bec16f06a5fbc9bf5ed3774c15
Ismael Celis <ismaelct@gmail.com>
2011-01-12 18:08:37 -0300

    Language namespace

--- app.rb
+++ app.rb
@@ -4,6 +4,78 @@ require 'bundler/setup'
 require 'sinatra/base'
 require "rdiscount"
 
+class Page
+  
+  include Enumerable
+  
+  attr_reader :path, :name
+  
+  def initialize(path)
+    @path = path
+    @children = []
+    process!
+  end
+  
+  def <<(child)
+    @children << child
+  end
+  
+  def each(&block)
+    @children.each &block
+  end
+  
+  def size
+    @children.size
+  end
+  
+  def subdir
+    @subdir ||= @path.sub(File.extname(@path), '')
+  end
+  
+  def has_subdir?
+    File.directory? subdir
+  end
+  
+  def url
+    @url ||= @path.split('views').last.sub(File.extname(@path), '')
+  end
+  
+  protected
+  
+  def process!
+    @name = File.basename(@path, File.extname(@path))
+  end
+end
+
+class Menu
+  
+  attr_reader :root
+  
+  def initialize(path, except = [])
+    @path, @except = path, except
+    @root = []
+    build! @root, @path
+  end
+  
+  def each(&block)
+    @root.each &block
+  end
+  
+  protected
+  
+  def build!(level, path)
+    Dir[path + '/*.mkd'].each do |p|
+      next if @except.include?(File.basename(p))
+      page = Page.new(p)
+      level << page
+      if page.has_subdir?
+        build!(page, page.subdir)
+      end
+    end
+  end
+  
+end
+
 class App < Sinatra::Base
   
   set :public, File.dirname(__FILE__) + '/public'
@@ -30,17 +102,26 @@ class App < Sinatra::Base
       end
     end
     
-    def sub_menu
-      erb @sub_menu.to_sym if @sub_menu
-    end
-    
-    def menu_item(name, path)
-      klass = 'current' if path == '/'+@current_path
-      %(<a href="#{path}" class="#{klass}">#{name}</a>)
-    end
-    
     def partial(page, options={})
       erb :"_#{page}", options.merge!(:layout => false)
+    end
+    
+    def menu
+      @menu ||= Menu.new(options.root + '/views/' + params[:lang], ['404.mkd']) if params[:lang]
+    end
+    
+    def build_menu(page)
+      klass = 'current' if page.url == '/'+@current_path
+      str = %(<li class="page">)
+      str << %(<a href="#{page.url}" class="#{klass}">#{page.name}</a>)
+      if page.size > 0
+        page.each do |child|
+          str << %(<ul>)
+          str << build_menu(child)
+          str << %(</ul>)
+        end
+      end
+      str << %(</li>)
     end
   end
   
@@ -52,7 +133,11 @@ class App < Sinatra::Base
     mkd 'index'
   end
   
-  get '/*' do
-    mkd params[:splat].first
+  get '/:lang' do
+    mkd params[:lang]
+  end
+  
+  get '/:lang/*' do
+    mkd File.join(params[:lang], params[:splat].first)
   end
 end
