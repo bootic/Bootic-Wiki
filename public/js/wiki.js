f76d729a52c127c505aa32ae28f2b32bdab53d45
Ismael Celis <ismaelct@gmail.com>
2012-02-23 19:19:40 +0000

    Search results can be navigated with up/down arrows

--- public/js/wiki.js
+++ public/js/wiki.js
@@ -57,3 +57,49 @@ var WikiSearch = (function () {
     search: search
   }
 })()
+
+/* jQuery behaviours
+------------------------------------------*/
+// Search results view
+;(function () {
+  var $template = $('<li><a class="link"><span class="title"></span><span class="desc"></span></a></li>')
+  $(document).bind('search:results', function (evt, pages) {
+    $('#results').html('')
+    $.each(pages, function (i, page) {
+      $template.clone()
+        .find('.link').attr('href', page.href).end()
+        .find('.title').html(page.title).end()
+        .find('.desc').html(page.description).end()
+        .appendTo('#results')
+    })
+  })
+  
+  var current_idx = -1;
+  function arrow (how_much) {
+    var $lis = $('#results li')
+    current_idx = current_idx + how_much
+    if(current_idx > $lis.length -1) current_idx = 0
+    if(current_idx < 0) current_idx = $lis.length -1
+    
+    var $row = $lis.eq(current_idx)
+    $('#results li.hover').removeClass('hover')
+    $row.addClass('hover')
+  }
+  
+  $('#search').keydown(function (e) {
+    if(e.keyCode == 38) { // up
+      arrow(-1)
+      return false
+    } else if(e.keyCode == 40) { // down
+      arrow(1)
+      return false
+    }
+  })
+  
+  $('#search_form').submit(function () {
+    var row = $('#results li.hover')
+    console.log(row)
+    if(row.length > 0) document.location.href = row.find('.link').attr('href')
+    return false
+  })
+})()
