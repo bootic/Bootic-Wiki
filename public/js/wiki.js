88b759cdb8bf6626e97e715a8d7a2082a962c80e
Ismael Celis <ismaelct@gmail.com>
2012-02-23 23:27:51 +0000

    Turned JS search into a jQuery plugin for portability

--- public/js/wiki.js
+++ public/js/wiki.js
@@ -1,69 +1,76 @@
 /* Search using Lunr.js
 USAGE: WikiSearch.search('foo', function (results) {...})
 -----------------------------------------------------------------*/
-var WikiSearch = (function () {
-  
-  var index = Lunr('pages', function () {
-    this.field('title', {multiplier: 10})
-    this.field('description')
-    this.field('keywords', {multiplier: 5})
-    this.field('headings', {multiplier: 8})
-    this.ref('id')
-  })
-  
-  var pages = {},
-      loaded = false;
-  
-  var load = function (callback) {
-    if(loaded){ callback(); return }
-    
-    $.getJSON('/index.json', function (json) {
+(function ($) {
+  var Search = function (url, $container) {
 
-      $.each(json, function (i, page) {
-        // add object to local cache
-        pages[page.url] = page
-        // add to Lunr index
-        try {
-	        index.add({
-	          id: page.url,
-	          description: (page.description || ''),
-	          title: page.title,
-	          headings: page.headings.join(' '),
-	          keywords: (page.keywords ? page.keywords.replace(/,/gi, '') : ''),
-	        })	          
-        } catch(e) {
-          console.log('error', e, page)
-        }
+    var index = Lunr('pages', function () {
+      this.field('title', {multiplier: 10})
+      this.field('description')
+      this.field('keywords', {multiplier: 5})
+      this.field('headings', {multiplier: 8})
+      this.ref('id')
+    })
+
+    var pages = {},
+        loaded = false;
+
+    var load = function (callback) {
+      if(loaded){ callback(); return }
+
+      $.getJSON(url, function (json) {
+
+        $.each(json, function (i, page) {
+          // add object to local cache
+          pages[page.url] = page
+          // add to Lunr index
+          try {
+  	        index.add({
+  	          id: page.url,
+  	          description: (page.description || ''),
+  	          title: page.title,
+  	          headings: page.headings.join(' '),
+  	          keywords: (page.keywords ? page.keywords.replace(/,/gi, '') : ''),
+  	        })	          
+          } catch(e) {
+            console.log('error', e, page)
+          }
+        })
+
+        loaded = true
+        callback()
       })
+    }
 
-      loaded = true
-      callback()
+    this.search = function (term, callback) {
+      load(function () {
+        var hits = [];
+        $.each(index.search(term), function (i, url) {
+          hits.push(pages[url])
+        })
+        if(callback != undefined) callback(hits)
+        $container.trigger('search:results', [hits]);
+      })
+    }
+  }
+  
+  $.fn.booticDocSearch = function (url) {
+    var engine = new Search(url, $(this))
+    return $(this).keyup(function (e) {
+      if(e.keyCode != 38 && e.keyCode != 40) engine.search($(this).val())
+      return false;
     })
   }
-  
-  var search = function (term, callback) {
-    load(function () {
-      var hits = [];
-      $.each(index.search(term), function (i, url) {
-        hits.push(pages[url])
-      })
-      if(callback != undefined) callback(hits)
-      $(document).trigger('search:results', [hits]);
-    })
-  }
-  
-  return {
-    load: load,
-    search: search
-  }
-})()
+})(jQuery);
+
+
 
 /* jQuery behaviours
 ------------------------------------------*/
 // Search results view
 ;(function () {
   var $template = $('<li><a class="link"><span class="title"></span><span class="desc"></span></a></li>')
-  $(document).bind('search:results', function (evt, pages) {
+  $('#search').bind('search:results', function (evt, pages) {
     $('#results').html('')
     $.each(pages, function (i, page) {
       $template.clone()
