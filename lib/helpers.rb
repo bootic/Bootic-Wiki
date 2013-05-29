efcf076317003d8efd106e91338a7d47da8354ad
Tomás Pollak <tomaspollak@gmail.com>
2013-05-29 00:27:58 -0400

    Fixed 404 bug.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,12 +2,12 @@ module Helpers
   def cache_long(seconds = 3600)
     response['Cache-Control'] = "public, max-age=#{seconds.to_i}"
   end
-  
+
   def mkd(page)
     @content = render_markup(page.body)
     pjax? ? @content : erb(:layout)
   end
-  
+
   def markdown_engine
     @markdown_engine ||= begin
       renderer = Redcarpet::Render::HTML.new(:with_toc_data => true)
@@ -15,35 +15,35 @@ module Helpers
       engine
     end
   end
-  
+
   def render_markup(markup)
     erb_content = ERB.new(markup).result(binding)
     markdown_engine.render(erb_content)
   end
-  
+
   def coderay(body)
     body.gsub!(/<pre>:::(\w+)?\s(.+?)?<\/pre>/im) do |match|
       "<pre class='CodeRay'>#{::CodeRay.encoder(:html).encode($2, $1)}</pre>"
     end
   end
-  
+
   def render_404
-    @content = RDiscount.new(File.read(File.join(settings.root, 'views', '404.mkd'))).to_html
+    @content = render_markup(File.read(File.join(settings.root, 'views', '404.mkd')))
     halt 404, erb(:layout)
   end
-  
+
   def partial(page, options={})
     erb :"_#{page}", options.merge!(:layout => false)
   end
-  
+
   def menu
     @menu ||= Page.root
   end
-  
+
   def pjax?
     !!request.env['HTTP_X_PJAX']
   end
-  
+
   def load_page(url, json = false)
     if @page = Page.find('/'+url)
       mkd @page
@@ -51,19 +51,19 @@ module Helpers
       render_404
     end
   end
-  
+
   def recursive_xml(xml, page, level = 1.0)
     xml.url do
         xml.loc url(page.url)
         xml.changefreq "weekly"
         xml.priority(page.sitemap_priority || level)
     end
-    
+
     page.each do |child|
       recursive_xml xml, child, level/2
     end if page.size > 0
   end
-  
+
   def build_menu(page, depth = 1)
     return '' unless page.in_menus?
     klass = 'current' if @page && page.url == @page.url
@@ -78,18 +78,18 @@ module Helpers
     end
     str << %(</li>)
   end
-  
+
   def current_url
     return '' unless @page
     "http://#{request.env['HTTP_HOST']}#{@page.url}"
   end
-  
+
   def development?
     ENV['RACK_ENV'] == 'development'
   end
-  
+
   def img(path, size = nil)
     ::IMAGES.fetch(path.sub(/^\//, ''))
   end
-  
+
 end
